9b892d25fd09853059022c0a5af84e05719f5886
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-05 20:52:16 +0200

    Change shopping path to shop

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: 'shopping',
+        path: 'shop',
         element: <ShoppingPage />,
       },
       {
