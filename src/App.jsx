c1664c6e825f5362cf9ae524f2aa4da0ef28c99a
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-07 16:08:12 +0200

    Create scroll to top component for page changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </>
