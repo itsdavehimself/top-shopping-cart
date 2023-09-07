dfe75237a00f970b07b64616fb0e38aa7e2aeb76
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-07 20:52:56 +0200

    Style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,13 @@ export default function Footer() {
       <div className={styles.top}>
         <div className={styles.about}>
           <h3>ABOUT THE PROJECT</h3>
-          This project was made as part of The Odin Project&apos;s JavaScript
-          curriculum. The purpose of this shopping cart project is to explore
-          and utilize ReactJS, React testing, and React routing in conjunction
-          with CSS modules. The site is made fully responsive to work and look
-          good both on mobile and desktop.
+          <p>
+            This project was made as part of The Odin Project&apos;s JavaScript
+            curriculum. The purpose of this shopping cart project is to explore
+            and utilize ReactJS, React testing, and React routing in conjunction
+            with CSS modules. The site is made fully responsive to work and look
+            good both on mobile and desktop.
+          </p>
         </div>
         <div className={styles.shop}>
           <h3>SHOP</h3>
