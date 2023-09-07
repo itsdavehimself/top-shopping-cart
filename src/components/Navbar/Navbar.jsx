b6064109f7e6ebc1d43e312dd4ba58dac0c3a08f
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-07 13:20:21 +0200

    Add navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { useEffect } from 'react';
 
 export default function Navbar() {
+  const location = useLocation();
+  const isShopPage = location.pathname === '/shop';
+
+  useEffect(() => {
+    const navbar = document.querySelector(`.${styles.navbar}`);
+    let lastScrollY = window.scrollY;
+
+    function handleScroll() {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        navbar.style.transform = 'translateY(-100%)';
+      } else {
+        navbar.style.transform = 'translateY(0)';
+      }
+
+      if (currentScrollY >= 1) {
+        navbar.style.backgroundColor = 'rgba(240, 240, 240, .2)';
+        navbar.style.backdropFilter = 'blur(1px)';
+        navbar.style.borderBottom = '#dadada solid 1px';
+      } else {
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.borderBottom = 'rgba(255, 255, 255, 0) solid 1px';
+        navbar.style.backdropFilter = 'blur(0px)';
+      }
+
+      lastScrollY = currentScrollY;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header className={styles.navbar}>
       <div className={styles['logo-container']}>Logo Here</div>
       <div className={styles['nav-btns']}>
         <Link to="home">
-          <button>Home</button>
+          <button className={styles.btn}>Home</button>
         </Link>
         <Link to="shop">
-          <button>Shop</button>
+          <button className={styles.btn}>Shop</button>
         </Link>
         <Link to="cart">
-          <button>Cart</button>
+          <button className={styles.btn}>Cart</button>
         </Link>
       </div>
     </header>
