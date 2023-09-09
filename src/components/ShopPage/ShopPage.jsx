07873525aecd7299067a0a75c23b448f605bed35
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-09 17:52:04 +0200

    Add total item count

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -6,7 +6,7 @@ import ErrorPage from '../ErrorPage/ErrorPage';
 
 export default function ShoppingPage() {
   const { data, loading, error } = FetchAPI(
-    'https://api.bestbuy.com/v1/products((search=Gaming&search=Desktops))?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=customerReviewCount.asc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=24&format=json',
+    'https://api.bestbuy.com/v1/products((search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card))?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=customerReviewCount.asc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=24&format=json',
   );
 
   if (loading) {
@@ -37,16 +37,19 @@ export default function ShoppingPage() {
             <p>PC Components</p>
           </div>
         </div>
-        <div className={styles.catalogue}>
-          {data.products.map((product) => (
-            <ItemCard
-              key={product.sku}
-              image={product.image}
-              product={product.name}
-              rating={product.customerReviewAverage}
-              price={product.regularPrice}
-            />
-          ))}
+        <div className={styles.products}>
+          <div className={styles.total}>{data.total} items</div>
+          <div className={styles.catalog}>
+            {data.products.map((product) => (
+              <ItemCard
+                key={product.sku}
+                image={product.image}
+                product={product.name}
+                rating={product.customerReviewAverage}
+                price={product.regularPrice}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
