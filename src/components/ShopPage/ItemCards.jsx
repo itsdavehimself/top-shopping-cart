98e445ad39f044ff84e8b3bee21669e63c10950e
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-09 13:46:48 +0200

    Add props and styling

--- src/components/ShopPage/ItemCards.jsx
+++ src/components/ShopPage/ItemCards.jsx
@@ -1,17 +1,30 @@
 import styles from './ItemCards.module.css';
+import PropTypes from 'prop-types';
 
-export default function ItemCard() {
+export default function ItemCard({
+  image,
+  product = 'Placeholder Product Title For Development Purposes',
+  rating = '5.0',
+  price = '159.99',
+}) {
   return (
     <div className={styles.card}>
-      <div className={styles.img}></div>
+      <div className={styles.img}>
+        <img src={image}></img>
+      </div>
       <div className={styles['info']}>
-        <p className={styles['product-name']}>
-          Brand and Product Name Goes in this Space
-        </p>
-        <div className={styles.rating}>Rating Goes Here</div>
-        <p className={styles.price}>$159.99</p>
+        <p className={styles['product-name']}>{product}</p>
+        <div className={styles.rating}>{rating}</div>
+        <p className={styles.price}>${price}</p>
         <button className={styles.btn}>ADD TO CART</button>
       </div>
     </div>
   );
 }
+
+ItemCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  product: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
