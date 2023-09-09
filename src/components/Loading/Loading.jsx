deaf6c4d7de43105f75b3dfec7e8561797d726cb
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-09 13:20:23 +0200

    Create loading page

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import styles from './Loading.module.css';
+
+export default function LoadingPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+}
