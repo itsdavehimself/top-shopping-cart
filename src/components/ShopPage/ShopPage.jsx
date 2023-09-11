1ada631d761f3e6edd5a0dba9c41d2b17a0c3fba
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-11 13:01:28 +0200

    Create category filter functionality

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -5,14 +5,23 @@ import FetchAPI from '../API/FetchAPI';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import { useState, useEffect } from 'react';
 import Pagination from '../Pagination/Pagination';
+
 export default function ShoppingPage() {
   const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState(
+    '(search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card)',
+  );
   const { data, loading, error } = FetchAPI(
-    `https://api.bestbuy.com/v1/products((search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card))?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=bestSellingRank.dsc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=24&page=${page}&format=json`,
+    `https://api.bestbuy.com/v1/products(${filter})?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=bestSellingRank.dsc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=15&page=${page}&format=json`,
   );
 
   const handlePageChange = (newPage) => {
     setPage(newPage);
+  };
+
+  const changeFilter = (event) => {
+    const filter = event.target.getAttribute('data-filter');
+    setFilter(filter);
   };
 
   useEffect(() => {
@@ -44,10 +53,44 @@ export default function ShoppingPage() {
         <div className={styles.categories}>
           <h3 className={styles['category-title']}>Categories</h3>
           <div className={styles['category-links']}>
-            <p>Gaming Computers</p>
-            <p>Gaming Monitors</p>
-            <p>Gaming Laptops</p>
-            <p>Graphic Cards</p>
+            <button
+              className={styles['category-btn']}
+              data-filter="(search=Gaming&search=Desktops)"
+              onClick={changeFilter}
+            >
+              Gaming Desktops
+            </button>
+            <button
+              className={styles['category-btn']}
+              data-filter="(search=Gaming&search=Monitors)"
+              onClick={changeFilter}
+            >
+              Gaming Monitors
+            </button>
+            <button
+              className={styles['category-btn']}
+              data-filter="(search=Gaming&search=Laptops)"
+              onClick={changeFilter}
+            >
+              Gaming Laptops
+            </button>
+            <button
+              className={styles['category-btn']}
+              data-filter="(search=Graphic&search=Cards)"
+              onClick={changeFilter}
+            >
+              Graphic Cards
+            </button>
+            {filter !==
+              '(search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card)' && (
+              <button
+                className={styles['clear-filter-btn']}
+                data-filter="(search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card)"
+                onClick={changeFilter}
+              >
+                X Clear Filter
+              </button>
+            )}
           </div>
         </div>
         <div className={styles.products}>
@@ -67,7 +110,7 @@ export default function ShoppingPage() {
           <div className={styles['page-component']}>
             <Pagination
               currentPage={page}
-              totalPages={Math.ceil(data.total / 24)}
+              totalPages={Math.ceil(data.total / 15)}
               onPageChange={handlePageChange}
             />
           </div>
