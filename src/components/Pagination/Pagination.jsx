7d34b8d28a99dfddb80bdcebca731d2387b50036
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-09-11 12:17:03 +0200

    Add pagination component

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import styles from './Pagination.module.css';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const minPage = Math.max(1, currentPage - 1);
+  const maxPage = Math.min(totalPages, currentPage + 1);
+
+  const pages = Array.from(
+    { length: maxPage - minPage + 1 },
+    (_, index) => minPage + index,
+  );
+
+  return (
+    <div className={styles.pagination}>
+      {minPage > 1 && (
+        <button className={styles['page-btn']} onClick={() => onPageChange(1)}>
+          1
+        </button>
+      )}
+      {minPage > 2 && <span className={styles.ellipsis}>...</span>}
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={currentPage === page ? styles.active : styles['page-btn']}
+        >
+          {page}
+        </button>
+      ))}
+      {maxPage < totalPages - 1 && <span className={styles.ellipsis}>...</span>}
+      {maxPage < totalPages && (
+        <button
+          className={styles['page-btn']}
+          onClick={() => onPageChange(totalPages)}
+        >
+          {totalPages}
+        </button>
+      )}
+    </div>
+  );
+};
+
+Pagination.propTypes = {
+  currentPage: PropTypes.string,
+  totalPages: PropTypes.string,
+  onPageChange: PropTypes.func,
+};
+
+export default Pagination;
--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -3,11 +3,24 @@ import LoadingPage from '../Loading/Loading';
 import ItemCard from './ItemCards';
 import FetchAPI from '../API/FetchAPI';
 import ErrorPage from '../ErrorPage/ErrorPage';
-
+import { useState, useEffect } from 'react';
+import Pagination from '../Pagination/Pagination';
 export default function ShoppingPage() {
+  const [page, setPage] = useState(1);
   const { data, loading, error } = FetchAPI(
-    'https://api.bestbuy.com/v1/products((search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card))?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=bestSellingRank.dsc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=24&format=json',
+    `https://api.bestbuy.com/v1/products((search=Gaming&search=Desktops)|(search=Gaming&search=Monitor)|(search=Gaming&search=Laptops)|(search=graphic&search=card))?apiKey=qhqws47nyvgze2mq3qx4jadt&sort=bestSellingRank.dsc&show=name,customerReviewAverage,customerReviewCount,image,regularPrice,sku&pageSize=24&page=${page}&format=json`,
   );
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [page]);
 
   if (loading) {
     return (
@@ -50,6 +63,13 @@ export default function ShoppingPage() {
                 price={product.regularPrice}
               />
             ))}
+          </div>
+          <div className={styles['page-component']}>
+            <Pagination
+              currentPage={page}
+              totalPages={Math.ceil(data.total / 24)}
+              onPageChange={handlePageChange}
+            />
           </div>
         </div>
       </div>
