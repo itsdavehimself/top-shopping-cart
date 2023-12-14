6d7b511bd0f9f7c23633273be227ec2b8ec08faf
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-14 10:31:21 -0600

    Create responsive styling

--- src/components/ShoppingCart/Cart.jsx
+++ src/components/ShoppingCart/Cart.jsx
@@ -28,46 +28,48 @@ export default function Cart() {
   };
 
   return (
-    <div className={styles['cart-container']}>
-      <div className={styles.cart}>
-        <h2 className={styles['cart-title']}>Your loot</h2>
-        {cart.length > 0 ? (
-          cart.map((cartItem) => (
-            <CartItem
-              key={cartItem.sku}
-              product={cartItem.product}
-              price={cartItem.price}
-              image={cartItem.image}
-              sku={cartItem.sku}
-            />
-          ))
-        ) : (
-          <div>You have no items in your cart.</div>
-        )}
-      </div>
-      <div className={styles['total-container']}>
-        <h2 className={styles['cart-title']}>Loot totals</h2>
-        <div className={styles['sub-total-container']}>
-          <div className={styles['sub-total-title']}>Sub-total</div>
-          <div className={styles['sub-total']}>${total}</div>
+    <div className={styles['cart-component']}>
+      <div className={styles['cart-container']}>
+        <div className={styles.cart}>
+          <h2 className={styles['cart-title']}>Your loot</h2>
+          {cart.length > 0 ? (
+            cart.map((cartItem) => (
+              <CartItem
+                key={cartItem.sku}
+                product={cartItem.product}
+                price={cartItem.price}
+                image={cartItem.image}
+                sku={cartItem.sku}
+              />
+            ))
+          ) : (
+            <div>You have no items in your cart.</div>
+          )}
         </div>
-        <div className={styles.delivery}>
-          <div className={styles['shipping-title']}>Shipping</div>
-          <div className={styles.free}>FREE</div>
-        </div>
-        <div className={styles['total-line-container']}>
-          <div className={styles['total-title']}>Total</div>
-          <div className={styles.total}>${total}</div>
-        </div>
-        <div className={styles['checkout-btn-container']}>
-          <button
-            className={styles['checkout-btn']}
-            onClick={handleCheckoutClick}
-          >
-            CHECKOUT
-          </button>
-          <div className={styles['confetti-container']}>
-            {isExploding && <ConfettiExplosion />}
+        <div className={styles['total-container']}>
+          <h2 className={styles['cart-title']}>Loot totals</h2>
+          <div className={styles['sub-total-container']}>
+            <div className={styles['sub-total-title']}>Sub-total</div>
+            <div className={styles['sub-total']}>${total}</div>
+          </div>
+          <div className={styles.delivery}>
+            <div className={styles['shipping-title']}>Shipping</div>
+            <div className={styles.free}>FREE</div>
+          </div>
+          <div className={styles['total-line-container']}>
+            <div className={styles['total-title']}>Total</div>
+            <div className={styles.total}>${total}</div>
+          </div>
+          <div className={styles['checkout-btn-container']}>
+            <button
+              className={styles['checkout-btn']}
+              onClick={handleCheckoutClick}
+            >
+              CHECKOUT
+            </button>
+            <div className={styles['confetti-container']}>
+              {isExploding && <ConfettiExplosion />}
+            </div>
           </div>
         </div>
       </div>
